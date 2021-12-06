468dd6e18d4abe06737a186b115355d65e1513a1
Leonard Anderweit <leonard.anderweit@gmail.com>
2021-12-06 21:58:23 +0100

    removed dead code

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
 from quadro import Quadro
 from constants import *
-from quadro import Quadro
-from time import sleep
-
-checksum = [0,0]
-value = 10000
-seaching = True
 
 q = Quadro()
 q.readConfig()
-print(q.readParameter(FAN1_FALLBACK_RPM))
+print(q.readParameter(FAN1_PWM))
 q.close()
