b3e75f8eae14dc5f9d24aba04b3e5fdf139e96b6
Leonard Anderweit <leonard.anderweit@gmail.com>
2022-03-14 16:22:56 +0100

    added default values

--- structure.py
+++ structure.py
@@ -1,20 +1,20 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import Enum
 
 
 @dataclass
 class FlowSensor:
-    ticks_per_liter: int
-    correction_factor: float
+    ticks_per_liter: int = 169
+    correction_factor: float = 0.0
 
 @dataclass
 class FanSetup:
-    hold_min_power: bool
-    start_boost: bool
-    min_percent: int
-    max_percent: int
-    fallback: int
-    graph_rpm: int
+    hold_min_power: bool = False
+    start_boost: bool = True
+    min_percent: int = 5
+    max_percent: int = 100
+    fallback: int = 100
+    graph_rpm: int = 2000
 
 class FanCtrlMode(Enum):
     PWM = 0
@@ -27,39 +27,39 @@ class FanCtrlMode(Enum):
 
 @dataclass
 class TempTargetMode:
-    temp_target: float
-    P: int
-    I: int
-    D1: int
-    D2: float
-    hysteresis: float
+    temp_target: float = 35.0
+    P: int = 0
+    I: int = 0
+    D1: int = 0
+    D2: float = 0.0
+    hysteresis: float = 0.0
 
 @dataclass
 class Curve_mode:
-    start_temp: int
-    temp: list[float]
-    percent: list[float]
+    start_temp: int = 28
+    temp: list[float] = field(default_factory=lambda: [27.0, 28.1, 28.9, 29.8, 30.6, 31.5, 32.29, 33.2, 34.0, 34.9, 35.7, 36.6, 37.4, 38.29, 39.1, 40.0])
+    percent: list[float] = field(default_factory=lambda: [0.0, 1.4, 2.8, 5.0, 8.0, 12.0, 16.8, 22.6, 29.2, 36.6, 45.0, 54.2, 64.4, 75.4, 87.2, 100.0])
 
 @dataclass
 class Fan:
-    mode: FanCtrlMode
-    pwm: int
-    temp_sensor: int
-    temp_target_vars: TempTargetMode
-    curve_mode_vars: Curve_mode
+    mode: FanCtrlMode = FanCtrlMode.PWM
+    pwm: int = 100
+    temp_sensor: int = -1
+    temp_target_vars: TempTargetMode = TempTargetMode()
+    curve_mode_vars: Curve_mode = Curve_mode()
 
 @dataclass
 class RGB:
-    brightness: int
-    on: bool
+    brightness: int = 255
+    on: bool = True
 
 @dataclass
 class QuadroConfig:
-    aquabus: int
-    flow_sensor: FlowSensor
-    temp_sensors: list[float]
-    fan_setups: list[FanSetup]
-    fans: list[Fan]
-    rgb: RGB
-    profile: int
+    aquabus: int = 28
+    flow_sensor: FlowSensor = FlowSensor()
+    temp_sensors: list[float] = field(default_factory=lambda: [0.0, 0.0, 0.0, 0.0])
+    fan_setups: list[FanSetup] = field(default_factory=lambda: [FanSetup(), FanSetup(), FanSetup(), FanSetup()])
+    fans: list[Fan] = field(default_factory=lambda: [Fan(), Fan(), Fan(), Fan()])
+    rgb: RGB = RGB()
+    profile: int = 1
 
